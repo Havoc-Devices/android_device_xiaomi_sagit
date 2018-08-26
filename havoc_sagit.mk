32916806c1403c39eb63c33857a406ccc12de30b
arian <arian.kulmer@web.de>
2018-08-26 21:02:02 +0200

    sagit: havocify

---
+++ havoc_sagit.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/xiaomi/sagit/full_sagit.mk)
+
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/havoc/config/common.mk)
+
+PRODUCT_NAME := havoc_sagit
+PRODUCT_DEVICE := sagit
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="sagit-user 8.0.0 OPR1.170623.027 V9.2.3.0.OCAMIEK release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/sagit/sagit:8.0.0/OPR1.170623.027/V9.2.3.0.OCAMIEK:user/release-keys
